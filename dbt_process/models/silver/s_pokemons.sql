f24efe9707f923139dd276b2ee0f37ae431b3d11
AndreBezerra19 <andrebezerra19@yahoo.com.br>
2025-03-04 15:20:34 -0300

    WIP change shources definition

--- dbt_process/models/silver/s_pokemons.sql
+++ dbt_process/models/silver/s_pokemons.sql
@@ -5,6 +5,6 @@ SELECT
     p.name,
     t.type_1 as main_type,
     t.type_2 as sec_type 
-FROM {{ source('main', 'b_pokemons') }} p
-LEFT JOIN {{ source('main', 'b_types') }} t
+FROM {{ source('b_pokemons') }} p
+LEFT JOIN {{ source('b_types') }} t
     ON p.id = t.id
